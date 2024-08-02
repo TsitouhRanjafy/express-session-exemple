173956001b2fa8893d99090a12756153c17566a9
RAMIARANJAFY Tsitohaina <ramiaranjafy@kali>
2024-08-02 16:15:58 +0200

    ajout commentaire

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Application , Request , Response } from 'express'
 import session from 'express-session';
 
 
+// ** interface pour notre session
 declare module 'express-session' {
     interface SessionData {
         name : string;
@@ -15,38 +16,43 @@ const PORT = 4000;
 
 // Configure session middleware
 app.use(session({
-        secret : "gfg-key",
+        secret : "notreSecret-key",
         resave : false,
         saveUninitialized : true,
-        // session expire
+        // session expire (30 second)
         cookie : { maxAge : 30000 }
     })
 );
 
 // set session in the '/' route
 app.get('/',(req : Request,res : Response)=>{
-    req.session.name = "johnDoe";
+    req.session.name = "Tsitohaina";
     res.send("Session data set");
 })
 
 // get session in the '/getSession' route
 app.get('/getSession',(req : Request,res : Response)=>{
 
+    // ** est ce que le session name est definie ?
     if (req.session.name){
+        // ** si oui
+        // ** afficher le session name
         res.json({
             status : "ok",
             session : req.session.name
         })
     } else {
+        // ** si non
+        // ** dit que le session est non definie ou expirer
         res.json({
             status : "ok",
-            session : "not found"
+            session : "non definie ou expirer"
         })
     }
 })
 
 
-
+// demarrer serveur
 app.listen(PORT,()=>{
     console.log(` _________ on est près _________`);
     
